78d40b6ca2569a16fbb0d1cd440c7f156781ae24
Paul John B. Parreno <paul.john.parreno@gmail.com>
2020-07-11 06:22:17 +0800

    committing recent stable changes to recyclerView implementation

--- app/src/main/java/com/pparreno/myweather/ui/main/adapters/MainAdapter.kt
+++ app/src/main/java/com/pparreno/myweather/ui/main/adapters/MainAdapter.kt
@@ -1,26 +1,46 @@
 package com.pparreno.myweather.ui.main.adapters
 
-import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.pparreno.myweather.R
 import com.pparreno.myweather.models.CityWeather
 
-class MainAdapter( data : List<CityWeather>) : RecyclerView.Adapter<MainItemViewHolder>() {
+class MainAdapter(var data: LiveData<List<CityWeather>>) : RecyclerView.Adapter<MainItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainItemViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.main_city_weather_item, parent, false) as ViewGroup
+
+        return MainItemViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return if (this.data.value != null) this.data.value!!.size else 0
     }
 
     override fun onBindViewHolder(holder: MainItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        val model = this.data.value?.get(position)
+
+        holder.tempText.text = model!!.main.temperature.toString()
+        holder.cityNameText.text = model.name;
+        holder.weatherStatusText.text = model.weather[0].main
+
     }
 }
 
 class MainItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val favoriteIcon : ImageView = itemView.findViewById(R.id.favorite_icon)
+    val weatherIcon : ImageView = itemView.findViewById(R.id.weather_icon)
+    val tempText : TextView = itemView.findViewById(R.id.temp_text)
+    val cityNameText : TextView = itemView.findViewById(R.id.city_text)
+    val weatherStatusText : TextView = itemView.findViewById(R.id.weather_text)
+
 
 }
--- app/src/main/java/com/pparreno/myweather/ui/main/fragments/MainFragment.kt
+++ app/src/main/java/com/pparreno/myweather/ui/main/fragments/MainFragment.kt
@@ -8,7 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.pparreno.myweather.R
+import com.pparreno.myweather.ui.main.adapters.MainAdapter
 import com.pparreno.myweather.ui.main.viewmodels.MainViewModel
 
 class MainFragment : Fragment() {
@@ -17,6 +20,10 @@ class MainFragment : Fragment() {
         fun newInstance() =
             MainFragment()
     }
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: MainAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     private val TAG: String = "MAINFRAGMENT"
     private lateinit var viewModel: MainViewModel
@@ -27,19 +34,37 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        var view = inflater.inflate(R.layout.main_fragment, container, false)
+        viewManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById<RecyclerView>(R.id.main_recyclerview).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+        }
+        return view;
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewAdapter = MainAdapter(viewModel.groupWeather)
+        recyclerView.adapter = viewAdapter
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.groupWeather.observe(this, Observer {
-            Log.d(TAG, "length of result: " + it.count)
+            Log.d(TAG, "length of result: " + it.size)
+            viewAdapter.notifyDataSetChanged()
         })
+
+
+
+
 
     }
 }
