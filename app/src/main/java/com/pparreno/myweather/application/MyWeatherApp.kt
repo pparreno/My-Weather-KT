8336e87774e54d03630c4c663b3e54c85288c626
Paul John B. Parreno <paul.john.parreno@gmail.com>
2020-07-11 10:36:59 +0800

    commiting recent changes to recyclerView refresh functionality

---
+++ app/src/main/java/com/pparreno/myweather/application/MyWeatherApp.kt
@@ -0,0 +1,42 @@
+package com.pparreno.myweather.application
+
+import android.app.Application
+import android.util.Log
+import com.pparreno.myweather.BuildConfig
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class MyWeatherApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    /** A tree which logs important information for crash reporting.  */
+    private class CrashReportingTree : Timber.Tree() {
+        override fun log(
+            priority: Int,
+            tag: String?,
+            message: String,
+            t: Throwable?
+        ) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+//            FakeCrashLibrary.log(priority, tag, message)
+//            if (t != null) {
+//                if (priority == Log.ERROR) {
+//                    FakeCrashLibrary.logError(t)
+//                } else if (priority == Log.WARN) {
+//                    FakeCrashLibrary.logWarning(t)
+//                }
+//            }
+        }
+    }
+}
--- app/src/main/java/com/pparreno/myweather/ui/main/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/pparreno/myweather/ui/main/viewmodels/MainViewModel.kt
@@ -1,12 +1,16 @@
 package com.pparreno.myweather.ui.main.viewmodels
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.*
 import com.pparreno.myweather.R
+import com.pparreno.myweather.models.CityWeather
+import com.pparreno.myweather.models.GroupWeatherResponse
 import com.pparreno.myweather.repository.Repository
 import com.pparreno.myweather.utils.Utils
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -15,15 +19,31 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     val cityWeather = liveData(Dispatchers.IO) {
         val retrievedCityWeather = repository.getCityWeather(getApplication<Application>().resources.getString(
             R.string.api_key), "Manila")
-
         emit(retrievedCityWeather)
     }
 
-    val groupWeather = liveData(Dispatchers.IO){
-        val cityIDs = Utils.getPredefinedCityIDs();
-        val retrievedCityWeather = repository.getGroupWeather(getApplication<Application>().resources.getString(
-            R.string.api_key), Utils.buildGroupCityQueryParam(cityIDs))
+    var groupWeather : MutableLiveData<List<CityWeather>> = liveData(Dispatchers.IO){
+        Timber.d("Starting fetching of group weather")
+        val retrievedCityWeather = callGroupWeatherService()
         emit(retrievedCityWeather.data)
+    } as MutableLiveData<List<CityWeather>>
+
+    fun refresh() {
+        val scope =  CoroutineScope(Dispatchers.IO)
+       scope.launch {
+           val retrievedCityWeather = callGroupWeatherService()
+           groupWeather.postValue(retrievedCityWeather.data)
+       }
     }
+
+    private suspend fun callGroupWeatherService(): GroupWeatherResponse {
+        val cityIDs = Utils.getPredefinedCityIDs();
+        return repository.getGroupWeather(
+            getApplication<Application>().resources.getString(
+                R.string.api_keyd
+            ), Utils.buildGroupCityQueryParam(cityIDs)
+        )
+    }
+
 
 }
